99fcdeed71c19abbc6a180afcff06d406e0cf597
Bahodirjon <Bahodirjon@gmail.com>
2024-12-16 13:52:23 +0200

    4-oy masalalar

---
+++ app.js
@@ -0,0 +1,83 @@
+//Boolen1
+// let a = prompt("a sonni kiriting");
+// if (a > 0) {
+//     console.log("Siz kiritgan son musbat:'Rost'");
+// } else {
+//     console.log("Siz kiritgan son musbat emas:'Yolgon");
+// }
+
+//Boolen2
+// let a = prompt("a sonni kiriting ");
+// if (a % 2 !== 0) {
+//     console.log("Siz kiritgan son toq:'Rost'")
+// } else {
+//     console.log("Yolgon")
+// }
+
+//Boolen3
+// let a = prompt("a sonni kiriting");
+// let b = prompt("b sonni kiriting");
+
+// if (a > 2 && b <= 3) {
+//   console.log("Rost");
+// } else {
+//   console.log("Yolgon");
+// }
+
+//Boolen4
+
+// let a = prompt("a sonni kiriting");
+// let b = prompt("a sonni kiriting");
+// let c = prompt("a sonni kiriting");
+
+// if (a <= b && b <= c) {
+//     console.log("Rost")
+// } else {
+//     console.log("Yolgon")
+// }
+
+//Boolen5
+// let a = prompt("a sonini kiriting:");
+// let b = prompt("b sonini kiriting:");
+
+// if ((a % 2 === 0 && b % 2 === 0) || (a % 2 !== 0 && b % 2 !== 0)) {
+//   console.log(" Rost");
+// } else {
+//   console.log(" Yolg'on");
+// }
+
+// Boolean6
+
+// let a = prompt("a sonini kiriting:");
+// let b = prompt("b sonini kiriting:");
+// let c = prompt("c sonini kiriting:");
+
+// if (a > 0 || b > 0 || c > 0) {
+//   console.log(" Rost");
+// } else {
+//   console.log(" Yolg'on");
+// }
+
+//Boolen7
+
+// let a = prompt("a sonni kiriting");
+// let b = prompt("b sonni kiriting");
+// let c = prompt("c sonni kiriting");
+
+// if (a == b && a == c) {
+//     console.log("Rost");
+// } else {
+//     console.log("yolgon");
+// }
+
+// Boolen8
+
+// let a = prompt("a sonni kiriting");
+// let b = prompt("b sonni kiriting");
+// let c = prompt("c sonni kiriting");
+
+// if (a == b || b == c || a == c) {
+//     console.log("Rost");
+// } else {
+//     console.log("yolgon");
+// }
